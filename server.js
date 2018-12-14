22643825d9fac87ad97f86a54d26e3836a6a3ae6
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-14 16:34:03 +0800

    add jsonfile

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const io = require('socket.io')();
+
+const jsonfile = require('jsonfile')
+
+const file = './chat.json'
+
+let chats = []
+
 io.on('connection', client => { 
-  client.on('event', data => {
-    console.log(data);
+  client.on('chat message', data => {
+    chats.push({message: data})
+    jsonfile.writeFile(file, chats)
+    .then(res => {
+      client.emit('send status', {status: 'success'})
+    })
+    .catch(() => {
+      client.emit('send status', {status: 'error'})
+    })
   });
-  client.on('disconnect', () => {});
+  client.on('disconnect', () => {
+
+  });
 });
+
 io.listen(3000);
