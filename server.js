6ba591a9ead20f50fe494206a5fc74bcd4306cf8
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-14 17:57:23 +0800

    read json

--- server.js
+++ server.js
@@ -4,14 +4,44 @@ const jsonfile = require('jsonfile')
 
 const file = './chat.json'
 
-let chats = []
+let CHATS = []
 
 io.on('connection', client => { 
+  const { id } = client
+
+  client.on('chat ready', () => {
+    jsonfile.readFile(file)
+    .then(obj => {
+      CHATS = obj
+      client.emit('chat list', {
+        status: 'success',
+        data: CHATS, 
+      })
+    })
+    .catch(() => {
+      client.emit('chat list', {
+        status: 'error',
+      })
+    } )
+  } )
+  
   client.on('chat message', data => {
-    chats.push({message: data})
-    jsonfile.writeFile(file, chats)
+    const timemap = Date.now()
+
+    const c = {
+      id,
+      timemap,
+      message: data,
+    }
+
+    CHATS.push(c)
+
+    jsonfile.writeFile(file, CHATS)
     .then(res => {
-      client.emit('send status', {status: 'success'})
+      client.emit('send status', {
+        status: 'success',
+        data: c, 
+      })
     })
     .catch(() => {
       client.emit('send status', {status: 'error'})
