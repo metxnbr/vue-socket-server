8af33236aa0e15d3bf2fbee61bd71b5d931af033
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-19 15:27:46 +0800

    model

--- model.js
+++ model.js
@@ -1,11 +1,10 @@
 const my = require('./my')
 
-module.exports.getAccessToken = function *(accessToken) {
-  console.log('getAccessToken');
-
-  return my('SELECT * FROM oauth_access_token WHERE access_token = ?',
-            [accessToken]
-  ).then( results => {
+const model = {
+  getAccessToken: async accessToken => {
+    const results = await my('SELECT * FROM oauth_access_token WHERE access_token = ?',
+      [accessToken]
+    )
     const result = results[0];
     return {
       accessToken: result.access_token,
@@ -13,79 +12,57 @@ module.exports.getAccessToken = function *(accessToken) {
       client: { id: result.client_id },
       user: { id: result.user_id },
     }
-  } )
-  
+  },
+
+  getClient: async (clientId, clientSecret) => {
+    const results = await my('SELECT * FROM oauth_client WHERE id = ? AND secret = ?', [clientId, clientSecret])
+    result = results[0]
+    return {
+      ...result,
+      grants: ['password'],
+    }
+  },
+
+  getUser: async (username, password) => {
+    const results = await my('SELECT * FROM user WHERE username = ? AND password = ?', [username, password])
+    return results[0]
+  },
+
+  saveToken: async (token, client, user) => {
+    const {
+      accessToken,
+      accessTokenExpiresAt,
+
+      refreshToken,
+      refreshTokenExpiresAt,
+    } = token
+
+    const saveAccessToken = {
+      access_token: accessToken,
+      expires_at: accessTokenExpiresAt,
+      client_id: client.id,
+      user_id: user.id,
+    }
+
+    const saveRefreshToken = {
+      refresh_token: refreshToken,
+      expires_at: refreshTokenExpiresAt,
+      client_id: client.id,
+      user_id: user.id,
+    }
+
+    const accessResults = await my('INSERT INTO oauth_access_token SET ?', saveAccessToken)
+    const refreshResults = await my('INSERT INTO oauth_refresh_token SET ?', saveRefreshToken)
+
+    return {
+      accessToken,
+      accessTokenExpiresAt,
+      refreshToken,
+      refreshTokenExpiresAt,
+      client: { id: client.id },
+      user: { id: user.id }
+    }
+  },
 }
 
-/**
- * Get client.
- */
-
-module.exports.getClient = async(clientId, clientSecret) => {
-  console.log('getClient');
-  const results = await my('SELECT * FROM oauth_client WHERE id = ? AND secret = ?', [clientId, clientSecret])
-  result = results[0]
-  return {
-    ...result,
-    grants: ['password'],
-  }
-  
-};
-
-/*
- * Get user.
- */
-
-module.exports.getUser = async(username, password) => {
-  console.log('getUser');
-
-  const results= await my('SELECT * FROM user WHERE username = ? AND password = ?', [username, password])
-  return results[0]
-};
-
-/**
- * Save token.
- */
-
-module.exports.saveToken = async(token, client, user) => {
-  console.log('saveAccessToken');
-
-  console.log('token', token);
-  console.log('client', client);
-  console.log('user', user);
-  
-
-  const {
-    accessToken,
-    accessTokenExpiresAt,
-
-    refreshToken,
-    refreshTokenExpiresAt,
-  } = token
-
-  const saveAccessToken = {
-    access_token: accessToken,
-    expires_at: accessTokenExpiresAt,
-    client_id: client.id,
-    user_id: user.id,
-  }
-
-  const saveRefreshToken = {
-    refresh_token: refreshToken,
-    expires_at: refreshTokenExpiresAt,
-    client_id: client.id,
-    user_id: user.id,
-  }
-
-  const accessResults = await my('INSERT INTO oauth_access_token SET ?', saveAccessToken)
-  const refreshResults = await my('INSERT INTO oauth_refresh_token SET ?', saveRefreshToken)
-
-  return {
-    accessToken,
-    accessTokenExpiresAt,
-    refreshToken,
-    refreshTokenExpiresAt,
-    client: { id: client.id },
-    user: { id: user.id }
-  }
-};
+module.exports = model
