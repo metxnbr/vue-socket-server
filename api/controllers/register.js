1a02839deae53a5010f5b3bd75e84337c93c2fc6
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-21 14:02:40 +0800

    verify 2

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -4,18 +4,17 @@ const my = require('../../utils/my');
 module.exports = async (req, res) => {
   const { username, password } = req.body
 
-  // verify
-  if (username.length === 0) throw 'We need to know your username!'
-  if (username.length < 3) throw 'username no less than 3'
-  if (username.length > 12) throw 'username no more than 12'
-
-  if (password.length === 0) throw 'please set your password'
-  if (password.length < 6) throw 'password no less than 6'
-  if (password.length > 16) throw 'password no more than 16'
-
-  const saltRounds = 10;
-
   try {
+    // verify
+    if (username.length === 0) throw { type: 'custom', message: 'We need to know your username!' }
+    if (username.length < 3) throw { type: 'custom', message: 'username no less than 3' }
+    if (username.length > 12) throw { type: 'custom', message: 'username no more than 12' }
+
+    if (password.length === 0) throw { type: 'custom', message: 'please set your password' }
+    if (password.length < 6) throw { type: 'custom', message: 'password no less than 6' }
+    if (password.length > 16) throw { type: 'custom', message: 'password no more than 16' }
+
+    const saltRounds = 10;
     const hash = await bcrypt.hash(password, saltRounds)
 
     const value = {
@@ -29,7 +28,15 @@ module.exports = async (req, res) => {
       status: 'success',
     })
   } catch (error) {
-    const { errno } = error
+    const { errno, type, message } = error
+
+    if (type === 'custom') {
+      res.json({
+        status: 'error',
+        message,
+      })
+    }
+
     if (errno === 1062) {
       res.json({
         status: 'error',
