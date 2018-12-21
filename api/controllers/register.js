afa3da255cea8fef2f7e19f2dfae29794c18b8c6
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-21 13:54:47 +0800

    verify

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -3,6 +3,16 @@ const my = require('../../utils/my');
 
 module.exports = async (req, res) => {
   const { username, password } = req.body
+
+  // verify
+  if (username.length === 0) throw 'We need to know your username!'
+  if (username.length < 3) throw 'username no less than 3'
+  if (username.length > 12) throw 'username no more than 12'
+
+  if (password.length === 0) throw 'please set your password'
+  if (password.length < 6) throw 'password no less than 6'
+  if (password.length > 16) throw 'password no more than 16'
+
   const saltRounds = 10;
 
   try {
@@ -14,7 +24,7 @@ module.exports = async (req, res) => {
     }
 
     const results = await my('INSERT INTO user SET ?', value)
-    
+
     res.json({
       status: 'success',
     })
