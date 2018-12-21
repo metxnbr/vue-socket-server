b91800284308a1687ac2fb84e290e5b4c4e8799f
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-21 11:20:13 +0800

    register

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -1,23 +1,36 @@
 const bcrypt = require('bcrypt');
+const my = require('../../utils/my');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { username, password } = req.body
   const saltRounds = 10;
-  bcrypt.hash(password, saltRounds).then(hash => {
+
+  try {
+    const hash = await bcrypt.hash(password, saltRounds)
+
     const value = {
       username,
       password: hash,
     }
-    connection.query('INSERT INTO user SET ?', value, (error, results) => {
-      if (error) {
-        res.json({
-          status: 'error',
-          message: error,
-        })
-      }
-      res.json({
-        status: 'success',
-      })
+
+    const results = await my('INSERT INTO user SET ?', value)
+    
+    res.json({
+      status: 'success',
     })
-  });
+  } catch (error) {
+    const { errno } = error
+    if (errno === 1062) {
+      res.json({
+        status: 'error',
+        message: 'username already exsit',
+      })
+    }
+
+    res.json({
+      status: 'error',
+      message: 'fail to register',
+    })
+
+  }
 }
